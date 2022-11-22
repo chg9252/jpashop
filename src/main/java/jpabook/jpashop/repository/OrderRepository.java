6125b3d1ff5c46b9b08a73220089e6c179b6a2c4
Naerok <chg9252@gmail.com>
2022-11-22 15:26:59 +0900

    JPA JPQL 동적쿼리

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,7 +3,11 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,8 +24,55 @@ public class OrderRepository {
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
+/*
+
+    public List<Order> findAll(OrderSearch orderSearch){
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        // 주문 상태 검색
+        if(orderSearch.getOrderStatus() != null){
+            if(isFirstCondition){
+                jpql += "where";
+                isFirstCondition = false;
+            }else {
+                jpql += "and";
+            }
+            jpql += "o.status = :status";
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if(isFirstCondition){
+                jpql += "where";
+                isFirstCondition = false;
+            }else{
+                jpql += "and";
+            }
+            jpql += "m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+
+        */
+/* 정리
+        jpql을 문자로 생성하는것은 이처럼 매우 번거롭고 오류를 찾기에도 힘들다.
+        mybatis 는 검색하기 등 동적쿼리 생성에 편함.
+        *//*
 
 
+    }
 
+*/
 
 }
