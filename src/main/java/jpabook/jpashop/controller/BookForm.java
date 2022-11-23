bbd9e08fec9df73ecf57150585735ed4a8de133c
Naerok <chg9252@gmail.com>
2022-11-23 17:47:06 +0900

    create) itemList

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    // 상품 공통속성
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    // 책에 관련된 속성
+    private String author;
+    private String isbn;
+
+
+
+
+
+}
